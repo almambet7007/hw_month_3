2c48c19a1d6486e76a22b18af199bca1311bf835
almambet7007 <137039149+almambet7007@users.noreply.github.com>
2023-07-23 19:02:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from aiogram.utils import executor
-from config import dp
+from config import  dp
+from handlers import  start, chat_actions, callback, fsm_form
 from database import sql_commands
-from handlers import start, chat_actions, callback, ban_actions_in_gang
 
 
 start.register_handlers_start(dp=dp)
+fsm_form.register_handler_fsm_form(dp=dp)
 callback.register_handlers_callback(dp=dp)
-# ban_actions_in_gang.filter_messages()
 chat_actions.register_handlers_chat_actions(dp=dp)
 async def on_startup(_):
     db = sql_commands.Database()
     db.sql_create_db()
-    print("Bot is ready to work!!!")
+    print("bot is ready")
 
 if __name__ == "__main__":
     executor.start_polling(dp,
-                           skip_updates=True,
+                           skip_updates= True,
                            on_startup=on_startup
                            )
