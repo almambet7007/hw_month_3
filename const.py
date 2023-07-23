6a90bcd4b0fc7f422d42b9e80e46ba566caa0d9c
almambet7007 <abdyldaevalmambet14@gmail.com>
2023-07-23 18:41:52 +0600

    hw_2

--- const.py
+++ const.py
@@ -1,10 +1,14 @@
 HELP_TEXT = """
-Comands:
+Commands:
 /start - start button
-/help - description of bots function
+/help - description of bots functions 
 /quiz - funny poll answers with facts
+/signup - to register your form in bot
+/profiles - to view other user's profile 
+
+
+To view your form click to the profile button.
 
 Note: you should not use curse words because it's not allowed in this chat group
 Otherwise you will be banned !!!
-
 """
